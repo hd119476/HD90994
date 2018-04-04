f39be1f88cda25a69559584c6ae3b72452786427
Kimi <kimi@Kimi.local>
2018-04-04 13:18:17 +0800

    Initial Commit

---
+++ HD90994Tests/HD90994Tests.swift
@@ -0,0 +1,36 @@
+//
+//  HD90994Tests.swift
+//  HD90994Tests
+//
+//  Created by Kimi on 2018/4/4.
+//  Copyright © 2018年 Kimi. All rights reserved.
+//
+
+import XCTest
+@testable import HD90994
+
+class HD90994Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
